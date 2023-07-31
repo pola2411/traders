d8c6d53306d92a6e530afff83dde4ff934cc9e8e
PaolaRosalesUp <paolarosales@uptradingexperts.com>
2023-07-31 10:56:55 -0600

    Se eliminan y sustituyen etiquetas obsoletas

--- resources/views/fundamentales/show.blade.php
+++ resources/views/fundamentales/show.blade.php
@@ -1,15 +1,17 @@
 @extends('index')
 
 @section('css')
-<style>
-    #indexUSD {
-        width: 100%;
-        height: 500px;
-    }
-</style>
+    <style>
+        #indexUSD {
+            width: 100%;
+            height: 500px;
+        }
+    </style>
 @endsection
 
-@section('title') Fundamentales @endsection
+@section('title')
+    Fundamentales
+@endsection
 
 @section('content')
     <div class="pagetitle">
@@ -21,28 +23,28 @@
             </ol>
         </nav>
     </div>
-    
+
     <section class="section">
-      
-     <center>
-    
-    <iframe src="https://sslecal2.investing.com?columns=exc_flags,exc_currency,exc_importance,exc_actual,exc_forecast,exc_previous&category=_employment,_economicActivity,_inflation,_credit,_centralBanks,_confidenceIndex,_balance,_Bonds&importance=1,2,3&features=datepicker,timezone,timeselector,filters&countries=25,6,5,72,35,7,43,4,12&calType=week&timeZone=40&lang=49" 
-    
-    width="880"
-    height="480"
-    frameborder="0"
-    allowtransparency="true"
-    marginwidth="0" 
-    marginheight="0">
-        
-    </iframe><div class="poweredBy" style="font-family: Arial, Helvetica, sans-serif;">
-    <span style="font-size: 11px;color: #333333;text-decoration: none;">Calendario económico en vivo cortesía de <a href="https://mx.investing.com/" rel="nofollow" target="_blank" style="font-size: 11px;color: #06529D; font-weight: bold;" class="underline_link">Investing.com México</a></span></div>    
-    
-    </center>    
-        
+
+        <div class="text-center">
+
+
+
+            <iframe
+                src="https://sslecal2.investing.com?columns=exc_flags,exc_currency,exc_importance,exc_actual,exc_forecast,exc_previous&category=_employment,_economicActivity,_inflation,_credit,_centralBanks,_confidenceIndex,_balance,_Bonds&importance=1,2,3&features=datepicker,timezone,timeselector,filters&countries=25,6,5,72,35,7,43,4,12&calType=week&timeZone=40&lang=49"
+                width="880" height="480" frameborder="0" allowtransparency="true" marginwidth="0" marginheight="0">
+
+            </iframe>
+            <div class="poweredBy" style="font-family: Arial, Helvetica, sans-serif;">
+                <span style="font-size: 11px;color: #333333;text-decoration: none;">Calendario económico en vivo cortesía de
+                    <a href="https://mx.investing.com/" rel="nofollow" target="_blank"
+                        style="font-size: 11px;color: #06529D; font-weight: bold;" class="underline_link">Investing.com
+                        México</a></span>
+            </div>
+
+        </div>
+
     </section>
-        
-    
 @endsection
 
 @section('preloader')
@@ -65,5 +67,4 @@
 @endsection
 
 @section('script')
-    
 @endsection
